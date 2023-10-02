849dddc498b9560995e5a1e2121e9644569eb513
Uttam <uttam@flare.network>
2023-10-03 00:18:55 +0530

    added solution of side-

--- test/Levels/the-rewarder/TheRewarder.t.sol
+++ test/Levels/the-rewarder/TheRewarder.t.sol
@@ -9,6 +9,7 @@ import {TheRewarderPool} from "../../../src/Contracts/the-rewarder/TheRewarderPo
 import {RewardToken} from "../../../src/Contracts/the-rewarder/RewardToken.sol";
 import {AccountingToken} from "../../../src/Contracts/the-rewarder/AccountingToken.sol";
 import {FlashLoanerPool} from "../../../src/Contracts/the-rewarder/FlashLoanerPool.sol";
+import {Exploit} from "../../../src/Contracts/the-rewarder/Exploit.sol";
 
 contract TheRewarder is Test {
     uint256 internal constant TOKENS_IN_LENDER_POOL = 1_000_000e18;
@@ -88,6 +89,10 @@ contract TheRewarder is Test {
         /**
          * EXPLOIT START *
          */
+        vm.warp(block.timestamp + 5 days);
+        vm.startPrank(attacker);
+        new Exploit(theRewarderPool, flashLoanerPool).exploit();
+        vm.stopPrank();
 
         /**
          * EXPLOIT END *
