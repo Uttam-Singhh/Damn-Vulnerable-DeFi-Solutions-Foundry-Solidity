1ad6e255ea7bc78f10c3282efd0a9c1e555aa905
Uttam <uttam@flare.network>
2023-10-02 16:34:04 +0530

    added solution of side-entrance

--- test/Levels/side-entrance/SideEntrance.t.sol
+++ test/Levels/side-entrance/SideEntrance.t.sol
@@ -5,6 +5,7 @@ import {Utilities} from "../../utils/Utilities.sol";
 import "forge-std/Test.sol";
 
 import {SideEntranceLenderPool} from "../../../src/Contracts/side-entrance/SideEntranceLenderPool.sol";
+import {Exploit} from "../../../src/Contracts/side-entrance/Exploit.sol";
 
 contract SideEntrance is Test {
     uint256 internal constant ETHER_IN_POOL = 1_000e18;
@@ -36,6 +37,10 @@ contract SideEntrance is Test {
         /**
          * EXPLOIT START *
          */
+        vm.startPrank(attacker);
+        Exploit exp = new Exploit(sideEntranceLenderPool);
+        exp.exploit();
+        vm.stopPrank();
 
         /**
          * EXPLOIT END *
